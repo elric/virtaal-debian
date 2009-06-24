elric/virtaal-debian

commit 76a9a885e23d7069e1d877f73b7b22d7c814dc48

Get .style.font_desc rather than .get_pango_context().get_font_description() - the latter doesn't have a font size if we didn't specify one, and this gives a size calculation of (0,0). This fixes bug 1000.
git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/virtaal@11711 54714841-351b-0410-a198-e36a94b762f5

virtaal/views/placeablesguiinfo.py
@@ -19,6 +19,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import gtk
+from gtk.gdk
 import pango
 from translate.storage.placeables import base, StringElem, general
 
@@ -228,10 +229,12 @@ class NewlineGUI(StringElemGUI):
     SCALE_FACTOR = 1.2 # Experimentally determined
 
     def create_repr_widgets(self):
-        lbl = gtk.Label('¶')
+        lbl = gtk.Label(u'¶')
         lbl.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('grey')) # foreground is light grey
-        lbl.modify_font(self.textbox.get_pango_context().get_font_description())
-        w, h = make_pango_layout(self.textbox, '¶', 100).get_pixel_size()
+        font_desc = self.textbox.style.font_desc
+        lbl.modify_font(font_desc)
+        self.textbox.get_pango_context().set_font_description(font_desc)
+        w, h = make_pango_layout(self.textbox, u'¶', 100).get_pixel_size()
         lbl.set_size_request(-1, int(h/1.2))
         self.widgets.append(lbl)
 

commit 8ccaf3c0233f3b05928021970a97c47d0c5c3113

Fix for minor syntax error introduced in r11711
git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/virtaal@11712 54714841-351b-0410-a198-e36a94b762f5

virtaal/views/placeablesguiinfo.py
@@ -19,7 +19,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import gtk
-from gtk.gdk
+import gtk.gdk
 import pango
 from translate.storage.placeables import base, StringElem, general
 